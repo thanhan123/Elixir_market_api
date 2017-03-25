afae8956b27dbe8e4496fb4378b208fb23170253
thanhan123 <thanhan.uit@gmail.com>
2017-03-25 23:20:48 +0700

    init project: api by Elixir

---
+++ web/models/market.ex
@@ -0,0 +1,21 @@
+defmodule MarketApi.Market do
+  use MarketApi.Web, :model
+
+  schema "markets" do
+    field :name, :string
+    field :phone, :string
+
+    has_many :products, MarketApi.Product
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:name, :phone])
+    |> validate_required([:name, :phone])
+  end
+end
